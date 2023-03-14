95e7e1a7cbb590f1a97bd4700a8f9313b9175c36
aubinaso <62132512+aubinaso@users.noreply.github.com>
2023-03-14 23:24:17 +0100

    Update aubin.svelte

--- src/aubin.svelte
+++ src/aubin.svelte
@@ -1,1 +1,12 @@
+<script>
+  let count = 0;
+  function increment () {
+    count+=1;
+  }
+</script>
+
 <p>Aubin Functionnality</p>
+<button on:click={increment}>
+  Clicked {count}
+  {count === 1 ? 'time' : 'times'}
+</button>
